4b452a49542ec18c6f6a8ce37a8c71f68d463540
csstar <Eric>
2024-12-18 02:32:27 +0800

    cargo mut

--- 01-hello/main.rs
+++ 01-hello/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-  println!("Hello, Rust.")
+  // mutable ¿É±äµÄ
+  let mut x = 1;
+  println!("x = {}", x);
+
+  x = 2;
+  println!("x = {}", x);
+
+  const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 }
